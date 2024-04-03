727dfc327944610a6431d5639386fd462846f0fe
James Robinson <james@jessian.io>
2024-04-03 11:46:44 +0100

    Add uploadFileToS3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
 	getObjectListFromS3: require('./s3/getObjectListFromS3'),
 	deleteObjectFromS3: require('./s3/deleteObjectFromS3'),
 	uploadTextToS3: require('./s3/uploadTextToS3'),
+	uploadFileToS3: require('./s3/uploadFileToS3'),
 	getJsonFromS3: require('./s3/getJsonFromS3'),
 	getTextFromS3: require('./s3/getTextFromS3'),
 
---
+++ s3/uploadFileToS3.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk')
+const fs = require('fs')
+
+// initialize S3 client
+const s3 = new AWS.S3()
+
+function uploadFileToS3(bucketName, path, filePath, { public = false, mimeType, cacheControl }) {
+	console.log('uploadFileToS3', bucketName, path)
+
+	// upload file to S3
+	return new Promise((resolve, reject) => {
+		const params = {
+			Bucket: bucketName,
+			Key: path,
+			Body: fs.readFileSync(filePath),
+		}
+
+		if (makePublic) params.ACL = 'public-read'
+		if (mimeType) params.ContentType = mimeType
+		if (cacheControl) params.CacheControl = cacheControl
+
+		s3.putObject(params, err => {
+			if (err) {
+				console.error('Upload error: ', err.message)
+				return reject(err)
+			}
+			console.log(`Uploaded ${bucketName}/${path}`)
+			resolve()
+		})
+	})
+}
+
+module.exports = uploadFileToS3
